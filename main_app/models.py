bbb80a6994804b779366757dd5b6c8c9d603dbab
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-07-25 11:59:18 -0400

    Add Venue model

--- main_app/models.py
+++ main_app/models.py
@@ -9,3 +9,16 @@ class Band(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'band_id': self.id})
+
+class Venue(models.Model):
+    name = models.CharField(max_length=150)
+    state = models.CharField(max_length=150)
+    city = models.CharField(max_length=150)
+    status = models.CharField(max_length=150)
+    email = models.CharField(max_length=150)
+    phone = models.IntegerField()
+    note = models.CharField(max_length=150)
+    band = models.ForeignKey(Band, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
