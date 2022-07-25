c2027a96790770cd899fd9fbc4a2f7b1738fba2c
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-07-25 13:30:07 -0400

    Alter Venue model so that status has choices to choose from

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.urls import reverse
 
+STATUSES = (
+    ('N', 'Not Contacted'),
+    ('C', 'Contacted'),
+    ('F', 'Followed Up With'),
+    ('B', 'Successfully Booked'),
+    ('X', 'Not Going To Work')
+)
+
 class Band(models.Model):
     name = models.CharField(max_length=150)
 
@@ -14,11 +22,11 @@ class Venue(models.Model):
     name = models.CharField(max_length=150)
     state = models.CharField(max_length=150)
     city = models.CharField(max_length=150)
-    status = models.CharField(max_length=150)
     email = models.CharField(max_length=150)
     phone = models.IntegerField()
     note = models.CharField(max_length=150)
+    status = models.CharField(max_length=30, choices=STATUSES, default=STATUSES[0][0])
     band = models.ForeignKey(Band, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return f"{self.get_status_display()} on {self.date}"
