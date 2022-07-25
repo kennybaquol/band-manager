91088ff6e6d54262be27b00557ab0ccea9a0e0f3
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-07-25 13:37:59 -0400

    Tweak and test altered Venue model in admin

--- main_app/models.py
+++ main_app/models.py
@@ -29,4 +29,4 @@ class Venue(models.Model):
     band = models.ForeignKey(Band, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.get_status_display()} on {self.date}"
+        return f"{self.get_status_display()} on {self.name}"
