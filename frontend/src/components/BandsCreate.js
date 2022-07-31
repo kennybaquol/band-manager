bc8aeefbcc08884eae55906b4ab0163757952438
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-07-31 17:08:27 -0400

    Add CSRF token-grabber to use POST method on Create route for Band

--- frontend/src/components/BandsCreate.js
+++ frontend/src/components/BandsCreate.js
@@ -5,22 +5,51 @@ export default function BandsDetail() {
     const [name, setName] = useState('')
     // const { id } = useParams()
 
-    useEffect(() => {
-        (async () => {
-            console.log('Running BandForm useEffect!')
-            // fetch('/main_app/bands/create')
-            //     .then(res => res.json())
-            //     .then((data) => {
-            //         console.log(data)
-            //         setBand(data)
-            //     })
-        })()
-    }, [])
+    // SOURCE: https://www.techiediaries.com/django-react-forms-csrf-axios/
+    // Get the csrf token to use when using the POST method
+    const getCookie = async (name) => {
+        let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = jQuery.trim(cookies[i]);
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+
+    }
 
     const handleChange = async (e) => {
-        e.preventDefault()
-        setName = ({...name, [e.target.name]: e.target.value})
+        // e.preventDefault()
+        setName({ ...name, [e.target.name]: e.target.value })
         console.log(name)
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const csrftoken = await getCookie('csrftoken');
+
+        const requestOptions = {
+            method: "POST",
+            headers: { 
+                "Content-Type": "application/json",
+                "X-CSRFToken": csrftoken
+            },
+            body: JSON.stringify({
+                name: name
+            }),
+        };
+        fetch("/main_app/create-band", requestOptions)
+            .then((res) => res.json())
+            .then((data) => {
+                // this.props.history.push("/room/" + data.code)
+                console.log(data)
+                console.log('LEL')
+            })
     }
 
     return (
@@ -28,11 +57,11 @@ export default function BandsDetail() {
             <h1>Add A Band</h1>
 
             {/* <!-- Leaving the action empty makes the form post to the same url used to display it --> */}
-            <form action="" method="POST">
-            Name: <input type="text" onChange={handleChange}/>
-            <input type="submit" value="Add Band!" class="btn" />
+            <form onSubmit={handleSubmit}>
+                Name: <input type="text" onChange={handleChange} />
+                <input type="submit" value="Add Band!" class="btn" />
             </form>
-                <br /><br />
+            <br /><br />
 
         </div>
     )
