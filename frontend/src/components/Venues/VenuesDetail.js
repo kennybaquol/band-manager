621e912b93bdf158b6351588f6e4d5fe5a720cd4
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-08-01 18:31:58 -0400

    Add conditional rendering based on delete button click

--- frontend/src/components/Venues/VenuesDetail.js
+++ frontend/src/components/Venues/VenuesDetail.js
@@ -3,7 +3,53 @@ import { Link, useParams } from 'react-router-dom'
 
 export default function VenuesDetail() {
     const [venue, setVenue] = useState([])
+    const [deletePressed, setDeletePressed] = useState(false)
     const { band_id, venue_id } = useParams()
+
+    // Upon submit, attempt to create a new Venue using a POST route
+    const handleDelete = async (e) => {
+        e.preventDefault()
+        console.log('delete button pressed AND confirmed')
+
+        // const csrftoken = await getCookie('csrftoken');
+        // const requestOptions = {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //         "X-CSRFToken": csrftoken
+        //     },
+        //     body: JSON.stringify(venue),
+        // };
+        // fetch(`/main_app/bands/${band_id}/venues/create-venue/`, requestOptions)
+        //     .then((res) => res.json())
+        //     .then((data) => {
+        //         // this.props.history.push("/room/" + data.code)
+        //         console.log(data)
+        //     })
+    }
+
+    // Upon submit, attempt to create a new Venue using a POST route
+    const handleClick = async (e) => {
+        e.preventDefault()
+        console.log('delete button pressed')
+        setDeletePressed(true)
+
+        // const csrftoken = await getCookie('csrftoken');
+        // const requestOptions = {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //         "X-CSRFToken": csrftoken
+        //     },
+        //     body: JSON.stringify(venue),
+        // };
+        // fetch(`/main_app/bands/${band_id}/venues/create-venue/`, requestOptions)
+        //     .then((res) => res.json())
+        //     .then((data) => {
+        //         // this.props.history.push("/room/" + data.code)
+        //         console.log(data)
+        //     })
+    }
 
     // Upon first load, get the details of the current venue
     useEffect(() => {
@@ -14,14 +60,14 @@ export default function VenuesDetail() {
                     setVenue(data)
                 })
         })()
-    }, [])    
+    }, [])
 
     // For testing*
     useEffect(() => {
         (async () => {
             console.log(venue)
         })()
-    }, [venue])  
+    }, [venue])
 
     return (
         <div>
@@ -40,9 +86,20 @@ export default function VenuesDetail() {
                                 <h3>Status: {venue.status}</h3>
                             </div>
                             <div class="card-action">
-                                <Link to={`/bands/${band_id}/venues/${venue.id}/edit`}>Edit</Link>
-                                <Link to={`/bands/${band_id}/venues/${venue.id}/delete`}>Delete</Link>
+                                <button><Link to={`/bands/${band_id}/venues/${venue.id}/edit`}>Edit</Link></button>
+                                {/* <Link to={`/bands/${band_id}/venues/${venue.id}/delete`}>Delete</Link> */}
+                                <button onClick={handleClick}><Link>Delete</Link></button>
                             </div>
+                            {deletePressed === true ?
+                                <>
+                                    <div class="card-action">
+                                        <h5>Are you SURE you want to delete {venue.name}?</h5>
+                                        <button onClick={handleDelete}><Link>Yes, Delete Forever</Link></button>
+                                    </div>
+                                </>
+                                :
+                                <h3></h3>
+                            }
                         </div>
                     </div>
                 </>
