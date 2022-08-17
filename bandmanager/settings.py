951a9286833eb58e781c210d87baa8297514ca89
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-08-17 15:26:49 -0400

    Fix React display issue

--- bandmanager/settings.py
+++ bandmanager/settings.py
@@ -33,8 +33,8 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = env('DJANGO_DEBUG') != 'False'
+DEBUG = True
+# DEBUG = env('DJANGO_DEBUG') != 'False'
 
 # Application definition
 
